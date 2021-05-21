e3b48f41bee10b74fae0b7a920f28720a80e0626
ido trabelssi <idoavtr@gmail.com>
2021-05-21 19:05:59 +0300

    started working on the table

--- AnomalyDetectionWebService/wwwroot/table.js
+++ AnomalyDetectionWebService/wwwroot/table.js
@@ -1,19 +1,23 @@
 ﻿//tb_ prefix to avoid collision with modle_list
 // use headerName for title
 const tb_columnDefs = [
-    { headerName:"Show In graph", flex: 2, checkboxSelection: true },
-    { field: "feature", flex: 2, sortable: true, filter: 'agTextColumnFilter', floatingFilter: true },
-    { field: "some information", flex: 2, sortable: true }
+    //{ headerName:"", flex: 1, checkboxSelection: true },
+    { field: "feature", sortable: true, filter: 'agTextColumnFilter', floatingFilter: true, checkboxSelection: true },
+    { field: "highest", sortable: true },
+    { field: "lowest", sortable: true },
+    { field: "average", sortable: true },
+    { field: "is anomaly", sortable: true },
+    { field: "reason", sortable: true }
 ];
 
 // specify the data [example]
 const tb_rowData = [
-    { "feature": "speed", "some information": "WOW"},
-    { "feature": "height", "some information": "Amazing"},
-    { "feature": "yaw", "some information": "ok"},
-    { "feature": "pitch", "some information": "sounds good"},
-    { "feature": "roll", "some information": "101.2"},
-    { "feature": "temp", "some information": "Boxter"}
+    { "feature": "speed", "highest": "WOW", "lowest": "PFF", "average": "100", "is anomaly": "NO", "reason": "--" },
+    { "feature": "height", "highest": "Amazing", "lowest": "WHAT", "average": "98", "is anomaly": "YES", "reason": "Linear Regerssion with B" },
+    { "feature": "yaw", "highest": "ok", "lowest": "LOW", "average": "99", "is anomaly": "NO", "reason": "--" },
+    //{ "feature": "pitch", "highest value": "sounds good"},
+    //{ "feature": "roll", "highest value": "101.2"},
+   // { "feature": "temp", "highest value": "Boxter"}
 ];
 
 // let the grid know which columns and what data to use
@@ -35,13 +39,34 @@ document.addEventListener('DOMContentLoaded', () => {
     new agGrid.Grid(tb_gridDiv, tb_gridOptions);
 });
 
-// NOT called if all the data changed by    gridOptions.api.setRowData(x)
+// NOT called if all the data changed by gridOptions.api.setRowData(x)
 function tb_onRowSelected(event) {
     //event.node.data.feature  speed/roll ...
     //event.node.isSelected()  true/false
 }
 
 
-// change all data by 
+// change all data by
 // var tb_x = [{ "feature": "speed", "some information": "WOW"}, ...]
 // tb_gridOptions.api.setRowData(tb_x);
+
+//average, highest value, lowest value, is anomaly, reason
+
+
+
+function update_data(values_dictionary) {
+    var tb_x = [];
+    for (let key in values_dictionary) {
+        var sum = 0;
+        var high = values_dictionary[key][0];
+        var low = values_dictionary[key][0];
+        var tmp;
+        for (let i = 0; i < values_dictionary[key].length; i++) {
+            (high < values_dictionary[key][i]) ? (high = values_dictionary[key][i]) : tmp = high;
+            (low > values_dictionary[key][i]) ? (low = values_dictionary[key][i]) : tmp = high;
+            sum += values_dictionary[key][i];
+        }
+        var avg = sum / values_dictionary[key].length;
+
+    }
+}
